6795a1b50d81f4997724e14dbcce5329bcca7b95
Sam <bezzantsam@gmail.com>
2023-01-06 13:23:22 -0800

    css

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+      <section className="chatbox">
+
+      </section>
+      
     </div>
   );
 }
